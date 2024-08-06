d2325d82cb0380a8fd6434f13dba8e4b9d81ba09
SohailPro12 <131702341+SohailPro12@users.noreply.github.com>
2024-08-06 18:58:54 +0200

    task 2

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -38,3 +38,29 @@ def filter_datum(fields: List,
         message = re.sub(rf'{field}=.+?{separator}',
                          f'{field}={redaction}{separator}', message)
     return message
+
+
+PII_FIELDS = (
+    "name",
+    "email",
+    "phone",
+    "ssn",
+    "password"
+)
+def get_logger() -> logging.Logger:
+    """
+    returns a logging.Logger object
+    """
+    # Create a logger named "user_data"
+    logger = logging.getLogger("user_data")
+    # Only log messages with info level or higher
+    logger.setLevel(logging.INFO)
+    # Don't send messages to other handlers
+    logger.propagate = False
+    # Create a console handler to output logs to the console
+    handler = logging.StreamHandler()
+    # Set the log message format
+    handler.setFormatter(RedactingFormatter(PII_FIELDS))
+    # Add the console handler to the logger
+    logger.addHandler(handler)
+    return logger
