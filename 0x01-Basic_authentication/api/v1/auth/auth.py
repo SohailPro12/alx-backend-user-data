49df864959faba6d529d879571c97070b91880d2
Sohail Charef <charefsohayl@gmail.com>
2024-08-07 10:20:43 +0000

    auth class setup

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -11,10 +11,19 @@ class Auth:
     Auth class
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        checks if path require auth
+        """
         return False
 
     def authorization_header(self, request=None) -> str:
+        """
+        gets the auth header field from the request
+        """
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
+        """
+        gets the current user
+        """
         return None
