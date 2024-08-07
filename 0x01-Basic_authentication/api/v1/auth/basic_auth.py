e4930f69266618fc45166fba7e3dc106e661be2e
Sohail Charef <charefsohayl@gmail.com>
2024-08-07 14:13:05 +0000

    decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """ Class Basic Auth
 """
-
+import base64
+import binascii
 from api.v1.auth.auth import Auth
 
 
@@ -22,3 +23,20 @@ class BasicAuth(Auth):
         if not authorization_header.startswith("Basic "):
             return None
         return authorization_header.split(" ")[1]
+
+    def decode_base64_authorization_header(
+            self,
+            base64_authorization_header: str,
+            ) -> str:
+        """
+        Decodes a base64-encoded authorization header.
+        """
+        if type(base64_authorization_header) == str:
+            try:
+                res = base64.b64decode(
+                    base64_authorization_header,
+                    validate=True,
+                )
+                return res.decode('utf-8')
+            except (binascii.Error, UnicodeDecodeError):
+                return None
