6691da26ce8be8c00faf317f8a5c6dccd1e2ca0f
Sohail Charef <charefsohayl@gmail.com>
2024-08-07 14:20:34 +0000

    extract user cred

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 """ Class Basic Auth
 """
+import re
 import base64
 import binascii
+from typing import Tuple, TypeVar
 from api.v1.auth.auth import Auth
 
 
@@ -40,3 +42,22 @@ class BasicAuth(Auth):
                 return res.decode('utf-8')
             except (binascii.Error, UnicodeDecodeError):
                 return None
+
+    def extract_user_credentials(
+            self,
+            decoded_base64_authorization_header: str,
+            ) -> Tuple[str, str]:
+        """
+        returns the user email and password from the Base64 decoded value.
+        """
+        if type(decoded_base64_authorization_header) == str:
+            pattern = r'(?P<user>[^:]+):(?P<password>.+)'
+            field_match = re.fullmatch(
+                pattern,
+                decoded_base64_authorization_header.strip(),
+            )
+            if field_match is not None:
+                user = field_match.group('user')
+                password = field_match.group('password')
+                return user, password
+        return None, None
