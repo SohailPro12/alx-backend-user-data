3c32d401b2bbbca49d19ba0cfc8fbbadaf905204
Sohail Charef <charefsohayl@gmail.com>
2024-08-12 12:00:51 +0000

    find a  user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -4,9 +4,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm.session import Session
 
 from user import Base, User
+
+from typing import Dict
 
 
 class DB:
@@ -42,4 +46,24 @@ class DB:
             print(f"The new user was not added because: {e}")
             self._session.rollback()
             raise
+        return user
+
+    def find_user_by(self, **kwargs: Dict[str, str]) -> User:
+        """
+        find a  user in the database by different attr
+
+        Raises:
+            error: NoResultFound: When no results are found.
+            error: InvalidRequestError: When invalid query arguments are passed
+
+        Returns:
+            User: First row found in the `users` table.
+        """
+        session = self._session
+        try:
+            user = session.query(User).filter_by(**kwargs).one()
+        except NoResultFound:
+            raise NoResultFound()
+        except InvalidRequestError:
+            raise InvalidRequestError()
         return user
