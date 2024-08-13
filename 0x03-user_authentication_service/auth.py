20cd6cc8fb07352023d3ceb0c92e214a007477d9
Sohail Charef <charefsohayl@gmail.com>
2024-08-13 09:34:19 +0000

    hashing password method

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Auth process module
+"""
+import bcrypt
+
+
+def _hash_password(password: str) -> bytes:
+    """
+    takes in a password string arguments and returns bytes.
+
+    The returned bytes is a salted hash of the input password
+
+    hashed with bcrypt.hashpw
+    """
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
