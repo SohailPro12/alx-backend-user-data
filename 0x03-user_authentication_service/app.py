8fff53df1b8f3cfd88157fa996bcefc70ae27d25
Sohail Charef <charefsohayl@gmail.com>
2024-08-15 09:28:50 +0000

    User Profile

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -62,17 +62,11 @@ def logout() -> str:
 @app.route("/profile", methods=["GET"], strict_slashes=False)
 def profile() -> str:
     """GET /profile
-    Return:
-        - A JSON payload containing the email if successful.
     """
-    # Get the session ID from the "session_id" cookie in the request
     session_id = request.cookies.get("session_id")
-    # Retrieve the user associated with the session ID
     user = AUTH.get_user_from_session_id(session_id)
-    # If no user is found, abort the request with a 403 Forbidden error
     if user is None:
         abort(403)
-    # Return the user's email as a JSON payload
     return jsonify({"email": user.email})
 
 
